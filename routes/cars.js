7b9440c9424f05e9e32ee43088b342d7de1a5d2d
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-09-03 17:54:16 +0530

    Update cars.js

--- routes/cars.js
+++ routes/cars.js
@@ -4,7 +4,57 @@ var Car = require('../models/Car');
 var error = require('../error');
 var multer = require('multer');
 
+var storage = multer.diskStorage({
+    destination:'../carbazarapp/src/assets/images',
+    filename: function (req, file, cb) {
+      var fileSplit = file.originalname.split(".");
+var filename = file.originalname; 
+var fileLength = fileSplit.length;
+var extension = fileSplit[fileLength-1];
+filename = filename.replace("."+extension,"-");
+cb( null, filename+ Date.now()+"."+extension);
+    }
+  })
+   
+  var upload = multer({ storage: storage }).single('images')
+  router.post('/addcar', function (req, res) {
+  console.log(req.file);
+  upload(req, res, function (err) {
+    if (err)
+     {
+      // An error occurred when uploading
+      console.log(err);
+    }
 
+    var newCar = new Car({
+      "user": req.body.userid,
+      "registration_no": req.body.registration_no,
+      "model": req.body.model,
+      "speedometer": req.body.speedometer,
+      "manufacturer": req.body.manufacturer,
+      "photopath":req.file.filename,
+      "cost": req.body.cost,
+    });
+
+
+    var currentDate = new Date();
+    newCar.created_at = currentDate;
+    newCar.updated_at = currentDate;
+
+    newCar.save(function (err, car) {
+      try {
+        if (err) return res.send({ "status": "Error", "message": "Registration Number already Exists" });
+        return res.send({ "status": "Success", "message": "Data Inserted", "cars": car, "path" : req.file.path });
+      }
+      catch (err) {
+        res.send({ "status": "Error", "message": err });
+        throw err
+      }
+    });
+
+    // Everything went fine
+  })
+});
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
