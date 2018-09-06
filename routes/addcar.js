58a88ac8215502b15899e597ff04718d94c43bfe
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-09-06 10:46:21 +0530

    Update addcar.js

--- routes/addcar.js
+++ routes/addcar.js
@@ -2,28 +2,28 @@ var express= require('express');
 var router= express.Router();
 var multer = require('multer');
 var Car = require('../models/Car');
-var storage = multer.diskStorage({
-    destination:'./images',
-    filename: function (req, file, cb) {
-      var fileSplit = file.originalname.split(".");
-var filename = file.originalname; 
-var fileLength = fileSplit.length;
-var extension = fileSplit[fileLength-1];
-filename = filename.replace("."+extension,"-");
-cb( null, filename+ Date.now()+"."+extension);
-    }
-  })
+// var storage = multer.diskStorage({
+//     destination:'./images',
+//     filename: function (req, file, cb) {
+//       var fileSplit = file.originalname.split(".");
+// var filename = file.originalname; 
+// var fileLength = fileSplit.length;
+// var extension = fileSplit[fileLength-1];
+// filename = filename.replace("."+extension,"-");
+// cb( null, filename+ Date.now()+"."+extension);
+//     }
+//   })
    
-  var upload = multer({ storage: storage }).single('images')
+//   var upload = multer({ storage: storage }).single('images')
 
 router.post('/add', function (req, res) {
   console.log(req.file);
-  upload(req, res, function (err) {
-    if (err)
-     {
-      // An error occurred when uploading
-      console.log(err);
-    }
+//   upload(req, res, function (err) {
+//     if (err)
+//      {
+//       // An error occurred when uploading
+//       console.log(err);
+//     }
 
     var newCar = new Car({
       "user": req.body.userid,
@@ -31,7 +31,8 @@ router.post('/add', function (req, res) {
       "model": req.body.model,
       "speedometer": req.body.speedometer,
       "manufacturer": req.body.manufacturer,
-      "photopath":req.file.filename,
+     // "photopath":req.file.filename,
+        "photopath":'test.jpg',
       "cost": req.body.cost,
     });
 
@@ -43,7 +44,8 @@ router.post('/add', function (req, res) {
     newCar.save(function (err, car) {
       try {
         if (err) return res.send({ "status": "Error", "message": "Registration Number already Exists" });
-        return res.send({ "status": "Success", "message": "Data Inserted", "cars": car, "path" : req.file.path });
+       // return res.send({ "status": "Success", "message": "Data Inserted", "cars": car, "path" : req.file.path });
+          return res.send({ "status": "Success", "message": "Data Inserted", "cars": car});
       }
       catch (err) {
         res.send({ "status": "Error", "message": err });
