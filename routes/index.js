e4f406ae8c6634395da49957ea758378dcc1c5c8
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-09-06 15:16:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const AWS = require ('aws-sdk');
+
+const BUCKET_NAME = 'demopro';
+const IAM_USER_KEY = 'AKIAINRWY3B6DJDKK3IQ';
+const IAM_USER_SECRET = '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';
+
 var multer = require('multer');
 var Car = require('../models/Car');
 var storage = multer.diskStorage({
@@ -50,8 +56,28 @@ router.post('/add', function (req, res) {
 
     newCar.save(function (err, car) {
       try {
-        if (err) return res.send({ "status": "Error", "message": "Registration Number already Exists" });
+       // if (err) return res.send({ "status": "Error", "message": "Registration Number already Exists" });
        // return res.send({ "status": "Success", "message": "Data Inserted", "cars": car, "path" : req.file.path });
+          let s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM_USER_SECRET,
+    Bucket: BUCKET_NAME
+  });
+  s3bucket.createBucket(function () {
+      var params = {
+        Bucket: BUCKET_NAME,
+        Key: file.name,
+        Body: file.data
+      };
+      s3bucket.upload(params, function (err, data) {
+        if (err) {
+          console.log('error in callback');
+          console.log(err);
+        }
+        console.log('success');
+        console.log(data);
+      });
+});
       }
       catch (err) {
         res.send({ "status": "Error", "message": err });
