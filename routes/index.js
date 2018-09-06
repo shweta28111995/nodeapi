baed4221d882812b57242657d3d297c086a1583d
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-09-06 11:00:40 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,146 +1,59 @@
-var express = require('express');
-var router = express.Router();
+var express= require('express');
+var router= express.Router();
+var multer = require('multer');
+var Car = require('../models/Car');
+var storage = multer.diskStorage({
+    destination:'../carbazarapp/src/assets/images',
+    filename: function (req, file, cb) {
+      var fileSplit = file.originalname.split(".");
+var filename = file.originalname; 
+var fileLength = fileSplit.length;
+var extension = fileSplit[fileLength-1];
+filename = filename.replace("."+extension,"-");
+cb( null, filename+ Date.now()+"."+extension);
+    }
+  })
+   
+  var upload = multer({ storage: storage }).single('images')
 
-var passport = require("passport");
-var async = require("async");
-var nodemailer = require("nodemailer");
-var crypto = require("crypto");
+router.post('/add', function (req, res) {
+  console.log(req.file);
+  upload(req, res, function (err) {
+    if (err)
+     {
+      // An error occurred when uploading
+      console.log(err);
+    }
 
-//reset
-var jwt = require('jwt-simple');
+    var newCar = new Car({
+      "user": req.body.userid,
+      "registration_no": req.body.registration_no,
+      "model": req.body.model,
+      "speedometer": req.body.speedometer,
+      "manufacturer": req.body.manufacturer,
+      "photopath":req.file.filename,
+      "cost": req.body.cost,
+    });
 
-var payload = { userId: 1 };
-var secret = 'fe1a1915a379f3be5394b64d14794932';
-var token = jwt.encode(payload, secret);
-var decode = jwt.decode(token, secret);
-const bodyParser = require('body-parser');
 
-//reset
+    var currentDate = new Date();
+    newCar.created_at = currentDate;
+    newCar.updated_at = currentDate;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    newCar.save(function (err, car) {
+      try {
+        if (err) return res.send({ "status": "Error", "message": "Registration Number already Exists" });
+        return res.send({ "status": "Success", "message": "Data Inserted", "cars": car, "path" : req.file.path });
+      }
+      catch (err) {
+        res.send({ "status": "Error", "message": err });
+        throw err
+      }
+    });
+
+    // Everything went fine
+  })
 });
-
-
-
-// router.post('/forgot', function(req, res, next){
-//   aync.waterfall([
-//     function(done){
-//       crypto.randomBytes(20, function(err, buf){
-//         var token = buf.toString('hex');
-//         done(err, token);
-//       });
-//     },
-//     function(token,done){
-//       User.findOne({ email: req.body.email},function(err,user){
-//         if(!user){
-//           req.flash('error','No account with that email address exists.');
-//           return res.redirect('/forgot');
-//         }
-//         user.resetPasswordToken=token;
-//         user.resetPasswordExpires = Date.now() + 360000; //1 hour
-
-//         user.save(function(err){
-//           done(err,token,user);
-//         });
-//       });
-//     },
-//     function(token, user,done){
-//       var smtpTransport=nodemailer.createTransport({
-//         service: 'Gmail',
-//         auth: {
-//           user: 'donotreply.testing.web@gmail.com',
-//           pass: '5jrC+7G~'
-//         }
-//       });
-//       var mailOptions = {
-//         to: user.email,
-//         from: 'donotreply.testing.web@gmail.com',
-//         subject: 'CarBazaar Password Reset',
-//         text: 'You are receiving this because you (or someone else) have requested the reset of the password.',
-//         'Please click on the following link, or paste this into the browser to complete the process': 'http://'
-//         + req.header.host + '/reset/' + token + '/n/n' +
-//         'If you did not request this, pleadse ignore this email and your password will remain unchanged.'
-//       };
-//       smtpTransport.sendMail(mailOptions, function(err){
-//         console.log('Email sent');
-//         req.flash('success', 'An email has been sent to ' + user.email + 'with further instructions.');
-//         done(err,'done');
-//       });
-//     }
-//   ], function(err){
-//     if(err) return next(err);
-//     res.redirect('/forgot');
-//   });
-// });
-
-// router.get('/reset/:token',function(req,res){
-//   User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}},
-//   function(err, user) {
-//     if (!user) {
-//       req.flash('error', 'Password reset token is invalid or has expired.');
-//       return res.redirect('/forgot');
-//     }
-//     res.render('reset', {token: req.params.token});
-//   });
-// });
-
-// router.post('/reset/:token', function(req, res) {
-//   async.waterfall([
-//     function(done) {
-//       User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-//         if (!user) {
-//           req.flash('error', 'Password reset token is invalid or has expired.');
-//           return res.redirect('back');
-//         }
-//         if(req.body.password === req.body.confirm) {
-//           user.setPassword(req.body.password, function(err) {
-//             user.resetPasswordToken = undefined;
-//             user.resetPasswordExpires = undefined;
-
-//             user.save(function(err) {
-//               req.logIn(user, function(err) {
-//                 done(err, user);
-//               });
-//             });
-//           })
-//         } else {
-//             req.flash("error", "Passwords do not match.");
-//             return res.redirect('back');
-//         }
-//       });
-//     },
-//     function(user, done) {
-//       var smtpTransport = nodemailer.createTransport({
-//         service: 'Gmail', 
-//         auth: {
-//           user: 'learntocodeinfo@gmail.com',
-//           pass: process.env.GMAILPW
-//         }
-//       });
-//       var mailOptions = {
-//         to: user.email,
-//         from: 'learntocodeinfo@mail.com',
-//         subject: 'Your password has been changed',
-//         text: 'Hello,\n\n' +
-//           'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-//       };
-//       smtpTransport.sendMail(mailOptions, function(err) {
-//         req.flash('success', 'Success! Your password has been changed.');
-//         done(err);
-//       });
-//     }
-//   ], function(err) {
-//     res.redirect('/users');
-//   });
-// });
-
-
-// //end password
-
-
-
 
 
 module.exports = router;
