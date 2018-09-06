2ec28761da7e2f4641c1a66497adb628fa397fe6
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-09-06 14:55:38 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var multer = require('multer');
 var Car = require('../models/Car');
 var storage = multer.diskStorage({
-    destination:'https://carbazarweb2.herokuapp.com/assets/images',
+    destination:'assets/images',
     filename: function (req, file, cb) {
       var fileSplit = file.originalname.split(".");
 var filename = file.originalname; 
